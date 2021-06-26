cb0d74bc0bf2a66a013661558ee33a7b2b08f449
masugata1995 <masugata1995@gmail.com>
2021-06-26 14:47:05 +0900

    create retirements function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
-  delete "/logout" => "sessions#destroy"
+  delete "/logout" => "sessions#destroy", as: :logout
+
+  resources :retirements
 
   resources :events do
     resources :tickets
